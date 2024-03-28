0e379563864b91d2037a14ca6f56dd771c457f79
Nedim Hairlahovic <nhairlahovic@evoila.com>
2024-03-28 12:47:37 +0100

    Initial commit

---
+++ src/main/java/com/example/springbootdemo/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String demo() {
+        return "demo";
+    }
+}
